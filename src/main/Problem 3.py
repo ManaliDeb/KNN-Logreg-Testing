4d85f9029063312589f3d247b3befe21b474ec5a
Manali Deb <Debook4@gmail.com>
2024-10-12 16:00:04 -0500

    setting up problem 3

---
+++ src/main/Problem 3.py
@@ -0,0 +1,15 @@
+import numpy as np
+from collections import Counter
+
+class KNNClassifier:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return np.array(predictions)
+
