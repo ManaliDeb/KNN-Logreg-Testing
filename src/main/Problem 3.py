85722957f9620d54b5246a4e50b59eee13b9a2df
Manali Deb <Debook4@gmail.com>
2024-10-12 16:01:43 -0500

    setting up predict function

--- src/main/Problem 3.py
+++ src/main/Problem 3.py
@@ -13,3 +13,13 @@ class KNNClassifier:
         predictions = [self._predict(x) for x in X]
         return np.array(predictions)
 
+    def _predict(self, x):
+        # distances between x and all examples
+        distances = np.linalg.norm(self.X_train - x, axis=1)
+        # k nearest neighbor indices
+        k_indices = np.argsort(distances)[:self.k]
+        # labels of the k nearest neighbors
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        # most common class label
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
